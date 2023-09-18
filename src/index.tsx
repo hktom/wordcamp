fae9646b400fbc0bc72f032b0a84887cea540bc0
tom hikari <tomprescott1565@gmail.com>
2023-09-18 10:05:20 +0200

    feature react router

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,30 @@
+/* eslint-disable react/no-deprecated */
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render( 
-  <div> This is a new react app hello </div>, 
-  document.getElementById('root')
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import CalendarPage from './pages/calendar';
+import MapPage from './pages/map';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <CalendarPage />,
+    },
+    {
+        path: '/map',
+        element: <MapPage />,
+    },
+]);
+
+ReactDOM.render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>,
+    document.getElementById('root'),
 );
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//     <React.StrictMode>
+//         <RouterProvider router={router} />
+//     </React.StrictMode>,
+// );
---
+++ src/pages/calendar.tsx
@@ -0,0 +1,7 @@
+import { Box } from '@mui/material';
+
+function CalendarPage() {
+    return <Box>hello</Box>;
+}
+
+export default CalendarPage;
