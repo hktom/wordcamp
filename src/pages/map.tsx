6781c8e5524fbeb8476855597efb2552a27bcddf
tom hikari <tomprescott1565@gmail.com>
2023-09-20 11:36:16 +0200

    remove unused package

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -2,9 +2,8 @@ import { Box, Typography } from "@mui/material";
 import MainLayout from "../layout/mainLayout";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { MAP_API_KEY } from "../helpers/apiKeys";
-import Map from "react-map-gl";
+
 import { useAppSelector } from "../store/store";
-import CMarker from "../component/cMarker";
 
 function MapPage() {
   const state = useAppSelector((state) => state.event);
@@ -12,7 +11,7 @@ function MapPage() {
   return (
     <MainLayout>
       <Box>
-        <Map
+        {/* <Map
           mapboxAccessToken={MAP_API_KEY}
           initialViewState={{
             longitude: 14.846,
@@ -22,12 +21,24 @@ function MapPage() {
           style={{ width: "100%", height: 500 }}
           mapStyle="mapbox://styles/mapbox/streets-v9"
         >
+          <Source
+            id="earthquakes"
+            type="geojson"
+            data="https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson"
+            cluster={true}
+            clusterMaxZoom={14}
+            clusterRadius={50}
+          >
+            <Layer {...clusterLayer} />
+            <Layer {...clusterCountLayer} />
+            <Layer {...unclusteredPointLayer} />
+          </Source>
           {state.events
             .filter((event) => event.latitude && event.longitude)
             .map((event) => (
               <CMarker key={event.id} {...event} />
             ))}
-        </Map>
+        </Map> */}
       </Box>
     </MainLayout>
   );
