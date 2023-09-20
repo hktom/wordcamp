c63fd76c6032bca89346d3aee9a5ac73c37ae9e6
tom hikari <tomprescott1565@gmail.com>
2023-09-20 07:25:11 +0200

    mapbox

--- src/helpers/apiKeys.ts
+++ src/helpers/apiKeys.ts
@@ -1,1 +1,3 @@
 export const API_URL = 'https://central.wordcamp.org/wp-json/wp/v2/wordcamps';
+
+export const MAP_API_KEY = "pk.eyJ1IjoidG9taGsiLCJhIjoiY2szZ3R2eG1rMDU2azNobXR5dXUzODRieiJ9.3AcxoJrp5yJtZTxPdqmDzw";
--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -1,12 +1,24 @@
 import { Box, Typography } from "@mui/material";
 import MainLayout from "../layout/mainLayout";
-import SmallCalendar from "../component/smallCalendar";
-import LargeCalendar from "../component/largeCalendar";
+import "mapbox-gl/dist/mapbox-gl.css";
+import { MAP_API_KEY } from "../helpers/apiKeys";
+import Map from "react-map-gl";
 
 function MapPage() {
   return (
     <MainLayout>
-      <Box></Box>
+      <Box>
+        <Map
+          mapboxAccessToken={MAP_API_KEY}
+          initialViewState={{
+            longitude: -122.4,
+            latitude: 37.8,
+            zoom: 2,
+          }}
+          style={{ width: "100%", height: 500 }}
+          mapStyle="mapbox://styles/mapbox/streets-v9"
+        />
+      </Box>
     </MainLayout>
   );
 }
